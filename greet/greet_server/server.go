a93800cffc53e1d991f4afcaf94a76752b035b20
Abin Thomas <abinmt@gmail.com>
2019-10-07 11:15:46 +0530

    Initial commit

--- greet/greet_server/server.go
+++ greet/greet_server/server.go
@@ -6,19 +6,25 @@ import (
 	"net"
 
 	"google.golang.org/grpc"
+
+	"github.com/abinmt/go-rpc/greet/greetpb"
 )
 
 type server struct{}
 
 func main() {
-	fmt.Println("Hello world")
-
+	fmt.Println("Hello World")
 	lis, err := net.Listen("tcp", "0.0.0.0:50051")
-
 	if err != nil {
 		log.Fatalf("Failed to listen: %v", err)
 	}
 
 	s := grpc.NewServer()
+
 	greetpb.RegisterGreetServiceServer(s, &server{})
+
+	if err := s.Serve(lis); err != nil {
+		log.Fatalf("Failed to serve: %v", err)
+	}
+
 }
